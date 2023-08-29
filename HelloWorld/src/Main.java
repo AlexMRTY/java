f84922c9e61eb8303ca5826938ec30f19d66394a
AlexMRTY <asifrezai261@gmail.com>
2023-08-29 14:49:17 +0200

    helloWorld project added

---
+++ HelloWorld/src/Main.java
@@ -0,0 +1,19 @@
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(gcdOfStrings("ABCABCABC", "ABCABC"));
+    }
+
+    public static String gcdOfStrings(String str1, String str2) {
+
+        String base = str1.length()>str2.length() ? str2 : str1;
+
+        
+        return base;
+    }
+}
+
+
+
