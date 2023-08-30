4b8dc94c6ec9d93a10743e9752b63d27504ec427
AlexMRTY <asifrezai261@gmail.com>
2023-08-30 08:44:35 +0200

    added Account class.

---
+++ HelloWorld/src/Account.java
@@ -0,0 +1,24 @@
+public class Account {
+    private double balance;
+    private String name;
+    public Account(double balance, String name) {
+        this.balance = balance;
+        this.name = name;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -3,15 +3,7 @@ import java.sql.SQLOutput;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(gcdOfStrings("ABCABCABC", "ABCABC"));
-    }
-
-    public static String gcdOfStrings(String str1, String str2) {
-
-        String base = str1.length()>str2.length() ? str2 : str1;
-
-        
-        return base;
+        Account myAccount = new Account(1000, "Asif");
     }
 }
 
