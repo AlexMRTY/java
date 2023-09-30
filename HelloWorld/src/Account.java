7ace5f3f3f84aed459870a700aea5f9c6923992d
AlexMRTY <asifrezai261@gmail.com>
2023-09-30 10:04:16 +0200

    labb3

--- HelloWorld/src/Account.java
+++ HelloWorld/src/Account.java
@@ -1,24 +1,42 @@
-public class Account {
-    private double balance;
-    private String name;
-    public Account(double balance, String name) {
-        this.balance = balance;
-        this.name = name;
-    }
+import java.util.Stack;
 
-    public double getBalance() {
-        return balance;
-    }
+class Solution {
+    public static String removeDuplicateLetters(String s) {
+        int[] lastIndex = new int[26];
+        for (int i=0 ; i<s.length() ; i++) {
+            lastIndex[s.charAt(i) - 'a'] = i+1;
+        }
 
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
+        boolean[] seen = new boolean[26];
 
-    public String getName() {
-        return name;
-    }
+        Stack<Character> stack = new Stack<>();
 
-    public void setName(String name) {
-        this.name = name;
+        stack.push(s.charAt(0));
+        seen[s.charAt(0) - 'a'] = true;
+        for (int i=1 ; i<s.length() ; i++) {
+            char c = s.charAt(i);
+            if (seen[c - 'a']) {continue;}
+//            if (stackTop > c) {                         // Check if stackTop is smaller than c.
+//                if (lastIndex[stackTop - 'a'] > i) {        // Check if stackTop exists later in the string s.
+//                    seen[stack.pop() - 'a'] = false;
+//                    stack.push(c);
+//                    seen[c - 'a'] = true;
+//                    continue;
+//                }
+//            }
+            while (stack.peek() > c && lastIndex[stack.peek() - 'a'] > i) {
+                    seen[stack.pop() - 'a'] = false;
+                    if (stack.isEmpty()) {break;}
+            }
+            stack.push(c);                    // If stackTop does not exist later in the string just push c to stack.
+            seen[c - 'a'] = true;
+
+        }
+
+        StringBuilder result = new StringBuilder();
+        while(!stack.isEmpty()) {
+            result.append((char)stack.pop());
+        }
+        return result.reverse().toString();
     }
 }
--- HelloWorld/src/Main.java
+++ HelloWorld/src/Main.java
@@ -3,7 +3,18 @@ import java.sql.SQLOutput;
 public class Main {
 
     public static void main(String[] args) {
-        Account myAccount = new Account(1000, "Asif");
+        System.out.println(test(121));
+    }
+
+    public static boolean test(int n) {
+        StringBuilder num = new StringBuilder();
+        num.append(n);
+
+        for (int i=0 ; i<num.length()-1 ; i++) {
+            if (num.charAt(i) != num.charAt(num.length()-i-1)) {return false;}
+        }
+
+        return true;
     }
 }
 
